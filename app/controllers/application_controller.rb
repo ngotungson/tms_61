d8586d3562ba9b12bdbd07df0f93420536163e57
nts <ngotungson0602@gmail.com>
2016-07-08 11:39:46 +0700

    Create namespace, dashboard page for supervisor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authenticate_supervisor!
+    unless current_user.supervisor?
+      flash[:danger] = t "controller.session.pleaseloginassupervisor"
+      redirect_to new_user_session_url
+    end
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,10 +4,24 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def create
-    super
+    if current_user.present?
+      flash[:success] = t "controller.session.success_signin"
+      if current_user.supervisor?
+        redirect_to supervisor_root_url
+      elsif current_user.trainee?
+        redirect_to root_url
+      end
+    else
+      flash[:danger] = t "controller.session.error_signin"
+      redirect_to new_user_session_url
+    end
   end
 
   def destroy
-    super
+    if current_user.present?
+      sign_out(current_user)
+      flash[:success] = t "controller.session.success_signout"
+      redirect_to root_url
+    end
   end
 end
